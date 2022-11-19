22e246c8d2dd22d5c6e0b8bb8f5a8144b9579ee3
blgcatAPCS <blgcat20@apcs.fitus.edu.vn>
2022-11-19 17:04:41 +0700

    add test cases

--- test/generate_input.py
+++ test/generate_input.py
@@ -1,40 +1,53 @@
 import os
 from random import randint
 
-def smallTestCase(file):
-    classNum = randint(1,3)
-    size = randint(10,40)
+def scaleDownClass(label):
+    s = set(label)
+    curLabel=1
+    while (len(s)>0):
+        curMin = min(s)
+        if (min(s)!=curLabel):
+            label[label==curMin]=curLabel
+        curLabel = curLabel+1
+        s.remove(curMin)
 
-    sumW, minW = 9,10000
-    weight = list()
+    return (curLabel-1, label)
+
+def smallTestCase(file):
+    classNum = randint(5,10)
+    size = randint(50,1000)
+
+    sumW, minW = 0, 1
+    weight = []
 
     for i in range(size):
-        newW = randint(1,10000)
+        newW = randint(1,1000)
         sumW = sumW + newW
         minW = min(minW, newW)
         weight.append(newW)
 
     value = list()
     for i in range(size):
-        value.append(randint(1,10000))
+        value.append(randint(1,1000))
 
     label = list()
     for i in range(size):
         label.append(randint(1,classNum))
 
     W = randint(minW, sumW)
+    classNum, label = scaleDownClass(label)
 
     f=open(file, 'w')
     f.write('{}\n'.format(W))
     f.write('{}\n'.format(classNum))
     for item in weight:
-        f.write(f"{item} ")
+        f.write(f"{item}, ")
     f.write("\n")
     for item in value:
-        f.write(f"{item} ")
+        f.write(f"{item}, ")
     f.write("\n")
     for item in label:
-        f.write(f"{item} ")
+        f.write(f"{item}, ")
     f.close()
 
-smallTestCase(os.path.dirname(os.path.dirname(__file__))+'\\data\\input\\input_004.txt')
+smallTestCase(os.path.dirname(os.path.dirname(__file__))+'\\data\\input\\input_010.txt')
